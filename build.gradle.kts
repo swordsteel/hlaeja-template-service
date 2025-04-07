bc2b4ca93fa5ec6af4c5de8502f8f971576f0b91
Swordsteel <swordsteel@gmail.com>
2025-04-07 17:20:41 +0200

    add hlaeja version catalog
    - remove repositories from build.gradle.kts
    - change to settings.gradle.kts
      - add dependency resolution management
      - add plugin management repositories
    - add catalog in gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,6 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
