e012b0507c8021f52272ffed4571460c40b15d04
Swordsteel <swordsteel@gmail.com>
2025-04-20 05:49:21 +0200

    first commit

---
+++ src/main/kotlin/ltd/hlaeja/HlaejaServiceApplication.kt
@@ -0,0 +1,11 @@
+package ltd.hlaeja
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HlaejaServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<HlaejaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ltd/hlaeja/HlaejaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ltd.hlaeja
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HlaejaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
